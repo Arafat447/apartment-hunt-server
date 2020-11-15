b1483316a84a89c3f46331279131bb8be3de7d9a
basir5101 <basir.bsmrstu@gmail.com>
2020-11-15 14:20:23 +0600

    get and post house work

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require ('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json())
+app.use(cors());
+const port = 5000;
+
+
+//apartmentHunt5101
+// hfzc0nS6wreGbJFs
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jo990.mongodb.net/apartmentHunt?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+
+  const houseCollection = client.db("apartmentHunt").collection("houses");
+
+  console.log('db - connected')
+
+// add house into database
+  app.post('/addHouse', (req, res) =>{
+    const house = req.body;
+    houseCollection.insertOne(house)
+    .then(result =>{
+        res.send(result.insertedCount);
+    })
+  })
+
+
+// get houses from database
+app.get('/houses', (req, res) =>{
+    houseCollection.find({})
+    .toArray((err, documents) =>{
+        res.send(documents);
+    }) 
+})
+
+
+});
+
+
+
+app.get('/', (req, res) =>{
+    res.send('I am working');
+})
+
+
+app.listen(port)
